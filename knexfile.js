3387618a7405e4bffe48a9bca709d8100d8b2674
fabricioandrade88 <fabricio.solutions13@gmail.com>
2024-09-08 22:28:10 +0200

    new2

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,17 @@
+const path = require("path")
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: path.resolve(__dirname, "src", "database", "database.db")
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
     },
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './src/database/knex/migrations' 
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './src/database/knex/migrations' 
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
     useNullAsDefault: true
   }
-
 };
