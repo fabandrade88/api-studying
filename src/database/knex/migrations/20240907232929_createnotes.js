44ecda7eae84d337265c01d733d94969ece7fdad
fabricioandrade88 <fabricio.solutions13@gmail.com>
2024-09-08 02:01:00 +0200

    migrate

--- src/database/knex/migrations/20240907232929_createnotes.js
+++ src/database/knex/migrations/20240907232929_createnotes.js
@@ -1,12 +1,11 @@
-
 exports.up = knex => knex.schema.createTable("notes", table => {
     table.increments("id");
     table.text("title");
     table.text("description");
     table.integer("user_id").references("id").inTable("users");
 
-    table.timestamp("create_at").default(knex.fn.now);
-    table.timestamp("update_at").default(knex.fn.now);
+    table.timestamp("create_at").default(knex.fn.now());
+    table.timestamp("update_at").default(knex.fn.now());
 
 });
 
